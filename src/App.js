e4c9612489a347881c3ca04cd3928d41c3838f01
Tuan Van Dinh <tuanvandinh@github.com>
2020-03-15 00:35:38 +0100

    add text

--- src/App.js
+++ src/App.js
@@ -7,20 +7,18 @@ function App() {
   const { t, i18n } = useTranslation();
 
   const handleClick = lang => i18n.changeLanguage(lang);
-  //  function handleClick(lang) {
-  //    console.log("pushed butto: ", lang);
-  //    return i18n.changeLanguage(lang);
-  //  }
 
   return (
     <div className="App">
-      <nav
-        style={{ width: "100%", padding: "2rem 0", backgroundColor: "gray" }}
-      >
-        <button onClick={() => handleClick("en")}>Englisch</button>
-        <button onClick={() => handleClick("de")}>Deutsch</button>
-      </nav>
       <header className="App-header">
+        <div className="Container-Button">
+          <button className="Button" onClick={() => handleClick("en")}>
+            Englisch
+          </button>
+          <button className="Button" onClick={() => handleClick("de")}>
+            Deutsch
+          </button>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>{t("header.intro")}</p>
       </header>
